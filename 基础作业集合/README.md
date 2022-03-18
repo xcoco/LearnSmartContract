8872fb907b0d27a95ceabcf62687c7590765b7c3
ChiFanLe <xcoco@users.noreply.github.com>
2022-03-18 15:20:42 +0800

    第四次作业-1

--- 基础作业集合/README.md
+++ 基础作业集合/README.md
@@ -1,5 +1,18 @@
 # 作业
 
+## W4_1 作业
+
+- 部署自己的 ERC20 合约 MyToken
+- 编写合约 MyTokenMarket 实现：
+  - AddLiquidity():函数内部调用 UniswapV2Router 添加 MyToken 与 ETH 的流动性
+  - buyToken()：用户可调用该函数实现购买 MyToken
+
+## W4_2 作业
+
+- 在上一次作业的基础上：
+  - 完成代币兑换后，直接质押 MasterChef
+  - withdraw():从 MasterChef 提取 Token 方法
+
 ## W3_1 作业
 
 - 发⾏⼀个 ERC20 Token：
---
+++ 基础作业集合/contracts/w4_1_MyTokenMarket.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./IUniswapV2Router01.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract MyTokenMarket {
+    using SafeERC20 for IERC20;
+    address weth;
+    address public token;
+    address public router;
+
+    constructor(
+        address _token,
+        address _weth,
+        address _router
+    ) public {
+        weth = _weth;
+        token = _token;
+        router = _router;
+    }
+
+    function AddLiquidity(uint256 tokenAmount) public payable {
+        require(tokenAmount > 0, "tokenAmount must be greater than 0");
+        require(msg.value > 0, "msg.value must be greater than 0");
+
+        IERC20(token).safeTransferFrom(msg.sender, address(this), tokenAmount);
+        IERC20(token).safeApprove(router, tokenAmount);
+        IUniswapV2Router01(router).addLiquidityETH{value: msg.value}(
+            token,
+            tokenAmount,
+            0,
+            0,
+            msg.sender,
+            block.timestamp
+        );
+    }
+
+    function BuyToken(uint256 miniTokenAmount) public payable {
+        require(msg.value > 0, "msg.value must be greater than 0");
+        require(miniTokenAmount > 0, "tokenAmount must be greater than 0");
+        address[] memory path = new address[](2);
+        path[0] = weth;
+        path[1] = token;
+        IUniswapV2Router01(router).swapExactETHForTokens(
+            miniTokenAmount,
+            path,
+            msg.sender,
+            block.timestamp
+        );
+    }
+
+    function ApproveMust() public {
+        IERC20(token).safeApprove(address(this), 100000000);
+        IERC20(token).safeTransferFrom(msg.sender, address(this), 1024);
+    }
+}
